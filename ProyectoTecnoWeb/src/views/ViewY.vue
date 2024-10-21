5e2850083d93cf0cc11fca8ec6de578531f5558d
Richter17F <84642305+Richter17F@users.noreply.github.com>
2024-10-20 20:33:20 -0400

    Agregar nuevas vistas

---
+++ ProyectoTecnoWeb/src/views/ViewY.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Esta es mi nueva vista</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NuevaVista',
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos aquí */
+  </style>
+  
